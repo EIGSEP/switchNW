8a929bd70eea3c6c28045472934b9d1686a3709f
Christian Hellum Bye <chbye@berkeley.edu>
2025-06-09 10:30:15 -0700

    fix pico import err

--- src/switch_network/__init__.py
+++ src/switch_network/__init__.py
@@ -1,5 +1,10 @@
 __author__ = "Charlie Tolley"
 __version__ = "0.0.1"
 
-from .switch import SwitchNetwork
-from . import pico_utils, testing
+try:
+   from .switch import SwitchNetwork
+   from . import testing
+except ImportError:
+   print("Running on Pico, skipping SwitchNetwork import")
+finally: 
+   from . import pico_utils
